8562d6e056eacc877684ebb41ad60243a7b50085
Liva Birkava <liva.birkava@startschool.org>
2025-11-24 11:20:00 +0200

    Create App component to render HabitTracker

---
+++ src/src/App.jsx
@@ -0,0 +1,9 @@
+import HabitTracker from './HabitTracker';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <HabitTracker />
+    </div>
+  );
+}
