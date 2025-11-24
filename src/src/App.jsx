9105901c6102cd9376acd967d0d0e7aaf4edc7fe
Liva Birkava <liva.birkava@startschool.org>
2025-11-24 15:19:44 +0200

    Delete src/src/App.jsx

--- src/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import HabitTracker from './HabitTracker';
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-gray-100">
-      <HabitTracker />
-    </div>
-  );
-}
